024ecbdd78aa5b8fe4edcd376728f30e74509f56
Nwaiwuchi10 <djnchrys@gmail.com>
2023-07-15 16:31:00 -0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
+
 import Navbar from "./components/Header/Navbar";
 import Home from "./pages/Home";
 import Footer from "./components/Footer/Footer";
